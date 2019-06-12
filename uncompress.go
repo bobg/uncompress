b4a0f5e129138c81633a39ced0e68c9a0ff8e451
Bob Glickstein <bobg@emphatic.com>
2019-06-12 08:07:23 -0700

    Discard bytes on Close. Add doc. Tag v1.0.0.

--- uncompress.go
+++ uncompress.go
@@ -1,3 +1,4 @@
+// Package uncompress permits seamless opening of compressed input files.
 package uncompress
 
 import (
@@ -7,7 +8,9 @@ import (
 	"strings"
 )
 
-var exts = map[string]string{
+// Exts maps a file suffix to the name of the program to use in OpenFile for uncompressing it.
+// Callers may modify Exts as needed.
+var Exts = map[string]string{
 	"Z":   "zcat",
 	"z":   "zcat",
 	"gz":  "zcat",
@@ -15,8 +18,14 @@ var exts = map[string]string{
 	"xz":  "xzcat",
 }
 
+// OpenFile opens the named file for reading.
+// If the file name has one of several special suffixes,
+// the resulting ReadCloser contains the output of an uncompressing program,
+// determined by Exts.
+// If the file does not have a suffix from Exts,
+// OpenFile falls back to using os.Open.
 func OpenFile(name string) (io.ReadCloser, error) {
-	for ext, prog := range exts {
+	for ext, prog := range Exts {
 		if strings.HasSuffix(name, "."+ext) {
 			cmd := exec.Command(prog, name)
 			r, err := cmd.StdoutPipe()
@@ -43,5 +52,15 @@ func (r rwrapper) Read(buf []byte) (int, error) {
 }
 
 func (r rwrapper) Close() error {
-	return r.cmd.Wait() // closes r.r
+	// Discard remaining bytes.
+	var buf [8192]byte
+	for {
+		_, err := r.r.Read(buf[:])
+		if err != nil {
+			break
+		}
+	}
+
+	// Closes r.r.
+	return r.cmd.Wait()
 }
